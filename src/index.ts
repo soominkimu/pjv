f57dccfa9575919b23ca9f182dbf1de3fa78d99d
Soomin K <soominkimu@gmail.com>
2022-05-08 01:15:49 +0900

    outdated versions for different dependencies supported

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@
 =============================================================================*/
 import fs from 'fs';
 import dotenv from 'dotenv';
+import path from 'path';
 import chalk from 'chalk';  // for TypeScript use version ^4
 import semver from 'semver';
 import { exec } from 'child_process';
@@ -28,10 +29,10 @@ const SEP  = chalk.gray(': ');  // separator between key and value
 const SEP0 = chalk.gray(':');
 const BULLET = '▫️ ';
 const MIN_PAD_VER = 6;  // minimum padding for version strings
-const SEMVERSIGN = new Map([
+const SEMVERSIGN = new Map([  // semverDiff
   ['major', '❌'],
   ['minor', '✅'],
-  ['patch', '✔️'],
+  ['patch', '✔️ '],
 ]);
 
 type TVMODE = 'all'|'scripts'|'dotenv'|'outdated';
@@ -50,13 +51,20 @@ interface TVersions {
   wanted:  string;
   latest:  string;
 };
+interface TOutdatedResult extends TVersions {
+  dependent: string;
+  location:  string;
+};
 interface TPackages {
+  dep:   string|undefined;
   name:  string;
   dev:   boolean;    // devDependencies
   ver:   string;     // version in package.json
   outd?: TVersions;  // outdated
 };
 const packVers: TPackages[] = [];
+let packageName = '';
+const dirBase = path.basename(process.cwd());
 
 const isObject = (o: Object) =>
   (typeof (o) === 'object') && !Array.isArray(o);  // array also is an 'object'
@@ -79,22 +87,23 @@ if (args[0]) {
   const { name, version, description } = require('../' + PACKAGE);  // own package
   const option = (arg[0] === '-')  // allow up to two '-' for the option prefix
     ? (arg[1] === '-')
-      ? arg.substring(2)  // long
+      ? arg[2]  // arg.substring(2)  // long
       : arg[1]  // short
     : '';
 
   if (option) {
-    switch (option.toUpperCase()) {
-      case 'E': vmode = 'dotenv';   break;
-      case 'S': vmode = 'scripts';  break;
-      case 'O': vmode = 'outdated'; break;
-      case 'V':
+    switch (option.toLowerCase()[0]) {
+      case 'e': vmode = 'dotenv';   break;
+      case 's': vmode = 'scripts';  break;
+      case 'o': vmode = 'outdated'; break;
+      case 'v':
         console.log(version);
         // console.log(process.env.npm_package_version);
         process.exit(0);
       default:
         console.log(chalk.yellowBright(name), chalk.greenBright('v' + version), description,
           '-', chalk.yellowBright('node'), chalk.greenBright(process.version));
+        console.log(' ', chalk.gray('by'), chalk.blue("SPACE") + chalk.green('TIME') + chalk.red('Q'));
         console.log(chalk.yellow("OPTIONS:"));
         let pad=7;  // set the longest length in HELP's long option
         HELP.forEach(arr => {
@@ -164,9 +173,9 @@ const logKeyValueObj = (obj: JSONObject, key: string) => {
   if (isDep && vmode === 'outdated') {
     const dev = key === 'devDependencies';
     Object.entries(o).forEach(([name, ver]) =>
-      packVers.push({ name, dev, ver: ver as string })
+      packVers.push({ dep: dirBase, name, dev, ver: ver as string })
     );
-    return;
+    return;  // just keep in packVers to print when outdated versions are fetched
   }
   logField(key);
   Object.entries(o).forEach(([k, val], i) => {
@@ -189,7 +198,11 @@ const logKeyValueObj = (obj: JSONObject, key: string) => {
 }
 
 const logKeyValue = (obj: JSONObject, key: string, pad: number) => {
-  console.log(BULLET, chalk.cyanBright(`${key.padEnd(pad)}`), SEP0, obj[key]);
+  let content = obj[key];
+  if (key === 'name' && packageName !== dirBase) {
+    content += '🚨' + chalk.redBright(' ≠../' + dirBase);
+  }
+  console.log(BULLET, chalk.cyanBright(`${key.padEnd(pad)}`), SEP0, content);
 }
 
 try {
@@ -220,9 +233,10 @@ if (vmode !== 'dotenv') {
         process.exit(1);
       }
       const json = JSON.parse(fs.readFileSync(rootPath + PACKAGE, 'utf8'));
+      packageName = json.name;
       console.log(
         chalk.white.bgGray(` ${PACKAGE}  `) +
-        chalk.black.bgWhite(` ${json.name} `) +
+        chalk.black.bgWhite(` ${packageName} `) +
         chalk.black.bgGreen(` ${json.version     || 'no version'} `) +
         chalk.green.bgBlack(` ${json.description || ''} `)
       );
@@ -237,7 +251,6 @@ if (vmode !== 'dotenv') {
         no.forEach(key => logKeyValue(json, key, pad));
       }
       ob.forEach(key => logKeyValueObj(json, key));
-
     });
   } catch (err) {
     console.error(err);
@@ -247,76 +260,89 @@ if (vmode !== 'dotenv') {
 /**
  * package versions
  * @param pack - package versions object
- * @param pad - package name padding
- * @param dp - package version padding
- * @param dv - outdated versions padding
+ * @param pd - padding 0: package nanme, 1: package version, 2: Current, 3: Wanted, 4: Latest
  */
-const logPackVers = (pack: TPackages[], pad: number, dp: number, dv: number) => {
+const logPackVers = (pack: TPackages[], pd: number[]) => {
   if (!pack.length)
     return;
-  const field = (pack[0].dev ? 'devDependencies' : 'dependencies').padEnd(pad + dp + 2);
+  const field = (pack[0].dev ? 'devDependencies' : 'dependencies').padEnd(pd[0] + pd[1] + 2);
   console.log(
-    chalk.black.bgCyan(` ◀︎ ${field.padEnd(pad+dp+2)} ▶︎ `),
-    chalk.black.bgWhite((dv > MIN_PAD_VER ? 'Current' : 'Cur').padStart(dv)),
-    chalk.black.bgGreen('Wanted'.padStart(dv)),
-    chalk.black.bgMagenta('Latest'.padStart(dv))
+    chalk.black.bgCyan(` ◀︎ ${field.padEnd(pd[0]+pd[1]+2)} ▶︎ `),
+    chalk.black.bgWhite((pd[2] > MIN_PAD_VER ? 'Current' : 'Cur').padStart(pd[2])),
+    chalk.black.bgGreen('Wanted'.padStart(pd[3])),
+    chalk.black.bgMagenta('Latest'.padStart(pd[4]))
   );
   pack.forEach((p, i) => {
-    const nk = ` ${p.name.padEnd(pad)} `;
-    const od = packVers.find(pk => pk.name === p.name)?.outd;
-    let sn = '';
-    if (od) {
-      const df = semver.diff(od.current || od.wanted, od.latest) || '';
+    const nk = ` ${p.name.padEnd(pd[0])} `;
+    let sn = '';  // sign
+    if (p.outd) {
+      const df = semver.diff(p.outd.current || p.outd.wanted, p.outd.latest) || '';
       const svsn = df && SEMVERSIGN.get(df);
       sn = svsn || df;
+      if (p.dep !== dirBase)
+        sn += ' ' + chalk.blueBright(p.dep);
     }
     console.log(
       logNumbering(i),
       p.name.includes('@')
-      ? chalk.redBright(nk) + SEP + chalk.blue(p.ver.padStart(dp))
-      : chalk.yellow(nk)    + SEP + chalk.cyan(p.ver.padStart(dp)),
-      od
-      ? chalk.white(         (od.current||'-').padStart(dv) ) + ' ' +
-        chalk.greenBright(   (od.wanted||' ').padStart(dv) )  + ' ' +
-        chalk.magentaBright( (od.latest||' ').padStart(dv) )
-      : '',
-      sn
+      ? chalk.redBright(nk) + SEP + chalk.blue(p.ver.padStart(pd[1]))
+      : chalk.yellow(nk)    + SEP + chalk.cyan(p.ver.padStart(pd[1])),
+      p.outd
+      ? chalk.white(         (p.outd.current||'-').padStart(pd[2]) ) + ' ' +
+        chalk.greenBright(   (p.outd.wanted ||' ').padStart(pd[3]) ) + ' ' +
+        chalk.magentaBright( (p.outd.latest ||' ').padStart(pd[4]) ) + ' ' + sn
+      : '✨'
     );
   });
 }
 
 /**
  * assumption that packVers was already built by reading package.json
+ * depent name from 'npm outdated' is the current folder name
  */
 if (vmode === 'outdated') {
+  // --------------------------------------------------------------------------------
   // takes time to run the external command
   exec('npm outdated --json', (error, stdout) => {  // stderr)
+  // --------------------------------------------------------------------------------
     if (stdout) {
       // console.log(stdout);
-      let pad=0;  // package name padding
-      let dp=MIN_PAD_VER;  // minimum padding for version string
-      let dv=MIN_PAD_VER;  // outdated version padding
-      packVers.forEach(p => {
-        if (p.name.length > pad) pad = p.name.length;
-        if (p.ver.length  > dp)  dp  = p.ver.length;
-      });
-      const json = JSON.parse(stdout);
+      const pd = Array(5).fill(MIN_PAD_VER, 1);  // minimum padding
+      pd[0]=0;  // minimum padding for package name
+      const json: TOutdatedResult[] = JSON.parse(stdout);
       Object.entries(json).forEach(([key, val]) => {
-        const { current, wanted, latest } = val as TVersions;
-        const fo = packVers.find(x => x.name === key);
-        if (fo) {
-          fo.outd = { current, wanted, latest };
-          if (current?.length > dv) dv = current.length;
-          if (wanted?.length  > dv) dv = wanted.length;
-          if (latest?.length  > dv) dv = latest.length;
-        } else {
-          console.error(key, "not found!");
+        const { current, wanted, latest } = val;
+        if (current?.length > pd[2]) pd[2] = current.length;
+        if (wanted?.length  > pd[3]) pd[3] = wanted.length;
+        if (latest?.length  > pd[4]) pd[4] = latest.length;
+        if (val.dependent === dirBase) {
+          const fo = packVers.find(x => x.name === key);
+          if (fo) {
+            fo.outd = { current, wanted, latest };
+          } else {
+            console.error(key, "not found!");
+          }
+        } else {  // dependent is another workspace
+          packVers.push({
+            dep: val.dependent,
+            name: key,
+            dev: false,
+            ver: '',
+            outd: { current, wanted, latest }
+          });
         }
       });
+      packVers.forEach(p => {
+        if (p.name.length > pd[0])
+          pd[0] = p.name.length;
+        if (p.ver.length > pd[1])
+          pd[1] = p.ver.length;
+      });
+      // partition array into prod dependencies and devDependencies
       const [pro, dev] = packVers.reduce<[TPackages[], TPackages[]]>(([pro, dev], k) =>
         (k.dev ? [pro, [...dev, k]] : [[...pro, k], dev]), [[], []]);
-      logPackVers(pro, pad, dp, dv);
-      logPackVers(dev, pad, dp, dv);
+      logPackVers(pro, pd);
+      logPackVers(dev, pd);
     }
     if (!error) {
       console.log(packVers.length ? '✨No outdated modules!' : "⚠️  No dependencies.");
